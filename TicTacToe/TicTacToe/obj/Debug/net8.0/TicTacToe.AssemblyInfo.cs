a874f84adfb86fdd289c8809e91babddceb125c1
Armen Margaryan <armenmargaryan89@gmail.com>
2024-06-10 23:03:04 +0400

    tt

--- TicTacToe/TicTacToe/obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/TicTacToe/obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d480ef76518be8926170d33a8c7bbf307bda92b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d4827a8d1fc221b3a9df91fd5c023d493e6f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
